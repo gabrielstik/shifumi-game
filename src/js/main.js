fbc88f19eb7a28bb163e6a3a7da2bae09dcef16c
gabrielstik <gabriel.stik@gmail.com>
2017-04-19 22:30:27 +0200

    Adding main menu & design images

---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('.intro p').classList.add('anim-intro');
+  document.querySelector('.play-buttons').classList.add('anim-comeBut');
+},false);
