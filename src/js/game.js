1f41251f88de6e715e53987d3c129f7138e68b94
gabrielstik <gabriel.stik@gmail.com>
2017-04-19 23:05:44 +0200

    Gulp multi js support & play mode functions

---
+++ src/js/game.js
@@ -0,0 +1,10 @@
+var soloButton = document.querySelector('.play-button-solo');
+var multiButton = document.querySelector('.play-button-multi');
+
+soloButton.addEventListener('click', function() {
+  console.log("solo");
+},false);
+
+multiButton.addEventListener('click', function() {
+  console.log("multi");
+},false);
