283810d8766dc861742968d7daaedf36bec910ec
gabrielstik <gabriel.stik@gmail.com>
2017-04-24 21:59:19 +0200

    :lipstick: Added game animation
    Signed-off-by: gabrielstik <gabriel.stik@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ var gulp         = require( 'gulp' ),
     // CSS
     gulp_cssnano = require('gulp-cssnano'),
     gulp_autoprefixer = require ( 'gulp-autoprefixer' ),
-    gulp_concat_css = require('gulp-concat-css'),
+    gulp_sass = require('gulp-sass'),
 
     // JS
     gulp_uglify  = require('gulp-uglify'),
@@ -28,11 +28,29 @@ var config = {
 // Running it by the command line : gulp
 gulp.task( 'default', [ 'watch' ], function() {} );
 
+// Scss into css, minifies and rename it "style.min.css"
+gulp.task('sass', function () {
+    return gulp.src(config.src + 'scss/*.scss')
+    .pipe(gulp_plumber({
+        errorHandler: gulp_notify.onError('SASS Erro  <%= error.message %>')
+    }))
+    .pipe(gulp_sourcemaps.init())
+    .pipe(gulp_sass({
+        outputStyle: 'compressed'}).on('error', gulp_sass.logError))
+    .pipe(gulp_sourcemaps.write())
+    .pipe(gulp_autoprefixer({
+        browsers: ['last 2 versions'],
+    cascade: false
+  }))
+    .pipe(gulp_rename('style.min.css'))
+    .pipe(gulp.dest(config.assets + 'css'))
+    // .pipe(gulp_notify('SASS has been compiled !'))
+});
+
 // Minifies and rename others css such as reset.css etc "library.min.css"
 gulp.task('styles', function () {
     return gulp.src(config.src + 'styles/*.css')
     .pipe(gulp_plumber({errorHandler: gulp_notify.onError('STYLES Erro  <%= error.message %>')}))
-    .pipe( gulp_concat_css('library.min.css') )
     .pipe(gulp_sourcemaps.init())
     .pipe(gulp_cssnano())
     .pipe(gulp_sourcemaps.write())
@@ -49,13 +67,12 @@ gulp.task('styles', function () {
 gulp.task( 'javascript', function()
 {
     return gulp.src( [
-          config.src + 'js/*.js'
+            './src/js/main.js',
         ] )
         .pipe(gulp_plumber({
             errorHandler: gulp_notify.onError("JS Error: <%= error.message %>")}))
         .pipe(gulp_sourcemaps.init())
         .pipe( gulp_uglify() )
-        .pipe( gulp_concat( 'main.min.js' ) )
         .pipe(gulp_sourcemaps.write())
         .pipe( gulp.dest(config.assets + 'js' ) );
 } );
@@ -66,11 +83,12 @@ gulp.task('imagemin', function()
     return gulp.src(config.src + 'img/*')
         .pipe(gulp_imagmin())
         .pipe(gulp.dest(config.assets + 'img'))
-        .pipe(gulp_notify('Images minified!'))
+        // .pipe(gulp_notify('Images minified!'))
 });
 
 // Watches files change and launches relatives tasks
-gulp.task( 'watch', ['javascript', 'imagemin'], function()
+gulp.task( 'watch', ['sass', 'javascript', 'imagemin'], function()
 {
+    gulp.watch(config.src + 'scss/**/*.scss', ["sass"]);
     gulp.watch(config.src + 'js/main.js', ["javascript"]);
 } );
--- src/js/game.js
+++ src/js/game.js
@@ -1,10 +1,13 @@
 var soloButton = document.querySelector('.play-button-solo');
 var multiButton = document.querySelector('.play-button-multi');
+var game = document.querySelector('.game');
 
 soloButton.addEventListener('click', function() {
+  game.classList.add('anim-comeGame');
   console.log("solo");
 },false);
 
 multiButton.addEventListener('click', function() {
+  game.classList.add('anim-comeGame');
   console.log("multi");
 },false);
