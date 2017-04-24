e272d451d0053ee8567bdd4c831706fd0e90d9e5
gabrielstik <gabriel.stik@gmail.com>
2017-04-25 00:17:16 +0200

    :sparkles: Full working solo gameplay
    Signed-off-by: gabrielstik <gabriel.stik@gmail.com>

--- src/js/game.js
+++ src/js/game.js
@@ -3,6 +3,8 @@ scoreP1 = document.querySelector('.score-p1');
 scoreP2 = document.querySelector('.score-p2');
 p1Score = 0;
 p2Score = 0;
+p1Played = document.querySelector('.played-p1');
+p2Played = document.querySelector('.played-p2');
 
 round();
 
@@ -10,6 +12,10 @@ function round() {
   p1Pressed = '';
   p2Pressed = '';
   if (mode == 'solo') {
+    var soloOrdi = document.querySelectorAll('.solo-ordi');
+    for (var i = 0; i < soloOrdi.length; i++) {
+      soloOrdi[i].style.display="none";
+    }
     document.body.onkeydown = function(e) {
       if (e.keyCode == 81 && p1Pressed == '') {
         p1Pressed = 'rock';
@@ -43,38 +49,52 @@ function round() {
 function solo(pressed) {
   var computer = Math.floor(Math.random()*3);
   if (computer == 0) {
+    p2Played.style.backgroundImage="url(assets/img/rock.png)";
     if (pressed == 'rock') {
-
+      p1Played.style.backgroundImage="url(assets/img/rock.png)";
     }
     else if (pressed == 'leaf') {
       p1Score++;
+      p1Played.style.backgroundImage="url(assets/img/leaf.png)";
     }
     else if (pressed == 'scissors') {
       p2Score++;
+      p1Played.style.backgroundImage="url(assets/img/scissors.png)";
     }
   }
   else if (computer == 1) {
+    p2Played.style.backgroundImage="url(assets/img/leaf.png)";
     if (pressed == 'rock') {
       p2Score++;
+      p1Played.style.backgroundImage="url(assets/img/rock.png)";
     }
     else if (pressed == 'leaf') {
-
+      p1Played.style.backgroundImage="url(assets/img/leaf.png)";
     }
     else if (pressed == 'scissors') {
       p1Score++;
+      p1Played.style.backgroundImage="url(assets/img/scissors.png)";
     }
   }
   else if (computer == 2) {
+    p2Played.style.backgroundImage="url(assets/img/scissors.png)";
     if (pressed == 'rock') {
       p1Score++;
+      p1Played.style.backgroundImage="url(assets/img/rock.png)";
     }
     else if (pressed == 'leaf') {
       p2Score++;
+      p1Played.style.backgroundImage="url(assets/img/leaf.png)";
     }
     else if (pressed == 'scissors') {
-
+      p1Played.style.backgroundImage="url(assets/img/scissors.png)";
     }
   }
-  scoreP1.innerHTML=p1Score;
-  scoreP2.innerHTML=p2Score;
+  setTimeout(function() {
+    scoreP1.innerHTML=p1Score;
+    scoreP2.innerHTML=p2Score;
+    p1Played.style.backgroundImage="url()";
+    p2Played.style.backgroundImage="url()";
+    round();
+  },3000);
 }
