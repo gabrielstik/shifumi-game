deb61a1a95f8ce6b23e49ca88bc0ce336b37a079
gabrielstik <gabriel.stik@gmail.com>
2017-04-24 23:35:01 +0200

    :sparkles: Solo gameplay
    Signed-off-by: gabrielstik <gabriel.stik@gmail.com>

--- src/js/game.js
+++ src/js/game.js
@@ -1,14 +1,80 @@
-var soloButton = document.querySelector('.play-button-solo');
-var multiButton = document.querySelector('.play-button-multi');
-var anticlick = document.querySelector('.anti-click');
-var game = document.querySelector('.game');
+mode = 'solo';
+scoreP1 = document.querySelector('.score-p1');
+scoreP2 = document.querySelector('.score-p2');
+p1Score = 0;
+p2Score = 0;
 
-soloButton.addEventListener('click', function() {
-  game.classList.add('anim-comeGame');
-  anticlick.style.display="none";
-},false);
+round();
 
-multiButton.addEventListener('click', function() {
-  game.classList.add('anim-comeGame');
-  anticlick.style.display="none";
-},false);
+function round() {
+  p1Pressed = '';
+  p2Pressed = '';
+  if (mode == 'solo') {
+    document.body.onkeydown = function(e) {
+      if (e.keyCode == 81 && p1Pressed == '') {
+        p1Pressed = 'rock';
+        solo(p1Pressed);
+  		}
+      else if (e.keyCode == 83 && p1Pressed == '') {
+        p1Pressed = 'leaf';
+        solo(p1Pressed);
+  		}
+      else if (e.keyCode == 68 && p1Pressed == '') {
+        p1Pressed = 'scissors';
+        solo(p1Pressed);
+  		}
+    }
+  }
+  else {
+    document.body.onkeydown = function(e) {
+      if (e.keyCode == 75) {
+
+  		}
+      else if (e.keyCode == 76) {
+
+  		}
+      else if (e.keyCode == 77) {
+
+  		}
+    }
+  }
+}
+
+function solo(pressed) {
+  var computer = Math.floor(Math.random()*3);
+  if (computer == 0) {
+    if (pressed == 'rock') {
+
+    }
+    else if (pressed == 'leaf') {
+      p1Score++;
+    }
+    else if (pressed == 'scissors') {
+      p2Score++;
+    }
+  }
+  else if (computer == 1) {
+    if (pressed == 'rock') {
+      p2Score++;
+    }
+    else if (pressed == 'leaf') {
+
+    }
+    else if (pressed == 'scissors') {
+      p1Score++;
+    }
+  }
+  else if (computer == 2) {
+    if (pressed == 'rock') {
+      p1Score++;
+    }
+    else if (pressed == 'leaf') {
+      p2Score++;
+    }
+    else if (pressed == 'scissors') {
+
+    }
+  }
+  scoreP1.innerHTML=p1Score;
+  scoreP2.innerHTML=p2Score;
+}
--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,20 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('.intro p').classList.add('anim-intro');
   document.querySelector('.play-buttons').classList.add('anim-comeBut');
 },false);
+
+var soloButton = document.querySelector('.play-button-solo');
+var multiButton = document.querySelector('.play-button-multi');
+var anticlick = document.querySelector('.anti-click');
+var game = document.querySelector('.game');
+
+soloButton.addEventListener('click', function() {
+  game.classList.add('anim-comeGame');
+  anticlick.style.display="none";
+  mode = 'solo';
+},false);
+
+multiButton.addEventListener('click', function() {
+  game.classList.add('anim-comeGame');
+  anticlick.style.display="none";
+  mode = 'multi';
+},false);
